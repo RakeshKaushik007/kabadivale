ce9815d1a8fa3375c8118ff760bd4f5876163a81
aum-singhal <pycoder77@gmail.com>
2023-03-28 22:22:19 +0530

    updated mongo connect

--- db/config.js
+++ db/config.js
@@ -2,4 +2,4 @@ import { set, connect } from 'mongoose';
 
 set('strictQuery', false);
 
-connect('mongodb://localhost:27017/kabadivala')
+connect('mongodb+srv://aum:ymx@p9m2@cluster0.3cbbu.mongodb.net')
